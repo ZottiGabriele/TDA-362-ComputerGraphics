24c60df5e63d94de1c06d74df1f34d3f1d2697ad
Gabriele Zotti <zotti@net.chalmers.se>
2019-11-20 15:21:41 +0100

    Lab 5 Task 1-2 Done

--- lab5-rendertotexture/lab5_main.cpp
+++ lab5-rendertotexture/lab5_main.cpp
@@ -132,8 +132,13 @@ struct FboInfo
 		///////////////////////////////////////////////////////////////////////
 		// Generate and bind framebuffer
 		///////////////////////////////////////////////////////////////////////
-		// >>> @task 1
-		//...
+		glGenFramebuffers(1, &framebufferId);
+		glBindFramebuffer(GL_FRAMEBUFFER, framebufferId);
+		// bind the texture as color attachment 0 (to the currently bound framebuffer)
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorTextureTarget, 0);
+		glDrawBuffer(GL_COLOR_ATTACHMENT0);
+		// bind the texture as depth attachment (to the currently bound framebuffer)
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthBuffer, 0);
 
 		// check if framebuffer is complete
 		isComplete = checkFramebufferComplete();
@@ -220,6 +225,10 @@ void initGL()
 	///////////////////////////////////////////////////////////////////////////
 	int w, h;
 	SDL_GetWindowSize(g_window, &w, &h);
+	const int numFbos = 5;
+	for (int i = 0; i < numFbos; i++) {
+		fboList.push_back(FboInfo(w, h));
+	}
 }
 
 void drawScene(const mat4& view, const mat4& projection)
@@ -311,8 +320,19 @@ void display()
 	///////////////////////////////////////////////////////////////////////////
 	// draw scene from security camera
 	///////////////////////////////////////////////////////////////////////////
-	// >>> @task 2
-	// ...
+	FboInfo &securityFB = fboList[0];
+	glBindFramebuffer(GL_FRAMEBUFFER, securityFB.framebufferId);
+	glViewport(0, 0, securityFB.width, securityFB.height);
+	glClearColor(0.2, 0.2, 0.8, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//render to texture
+	glUseProgram(shaderProgram);
+	
+	drawScene(securityCamViewMatrix, securityCamProjectionMatrix);
+	
+	labhelper::Material& screen = landingpadModel->m_materials[8];
+	screen.m_emission_texture.gl_id = securityFB.colorTextureTarget;
 
 	///////////////////////////////////////////////////////////////////////////
 	// draw scene from camera
